fcfb26e4d731411b9a918ba71e026abdc3c5fcaa
Filip Józwiak <filip@MSI>
2024-04-16 20:18:59 +0200

    Dodano funkcje

--- functions.js
+++ functions.js
@@ -1,15 +1,83 @@
+// Napisz funkcję, która zwraca sumę dwóch liczb.
+function sum(a, b) {
+    return a + b;
+}
+
+// Napisz funkcję, która zwraca największą liczbę z podanej tablicy.
+function largestElement(tab) {
+    let max = tab[0];
+    tab.forEach(element => {
+        if (element > max) {
+            max = element;
+        }
+    });
+    return max;
+}
+
+// Napisz funkcję, która usuwa duplikaty z tablicy.
+function removeDuplicats(tab) {
+    let set = new Set();
+    tab.forEach(element => {
+        set.add(element);
+    })
+    return Array.from(set);
+}
+
+// Napisz funkcję, która zamienia wszystkie litery w ciągu znaków na wielkie litery.
+function toUpper(text) {
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+        result += text.charAt(i).toUpperCase();
+    }
+    return result;
+}
+
+// Napisz funkcję, która zwraca średnią arytmetyczną elementów w tablicy liczb.
+function meanValue(tab) {
+    let _sum = 0;
+    let _items = 0;
+    tab.forEach(element => {
+        _sum += element;
+        _items++;
+    })
+    if (_items == 0) {
+        return 0;
+    } else {
+        return _sum / _items;
+    }
+}
+
+// Napisz funkcję, która sprawdza, czy dany ciąg znaków jest palindromem.
+function isPalindrome(text) {
+    for (let i = 0; i < text.length / 2 - 1; i++) {
+        if (text.charAt(i) != text.charAt(text.length - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Napisz funkcję, która zwraca n-ty element ciągu Fibonacciego.
+function fibonacci(n){
+    if (n < 2){
+        return 1;
+    } else{
+        return fibonacci(n-1) + fibonacci(n - 2);
+    }
+}
+
 function BubbleSort(tab) {
     let sorted = true
-    for(let i = 0; i< tab.length; i++){
-        for (let j = 0; j < tab.length - 1; j++){
-            if (tab[j] > tab[j+1]){
+    for (let i = 0; i < tab.length; i++) {
+        for (let j = 0; j < tab.length - 1; j++) {
+            if (tab[j] > tab[j + 1]) {
                 let _temp = tab[j];
-                tab[j] = tab[j+1];
-                tab[j+1] = _temp;
+                tab[j] = tab[j + 1];
+                tab[j + 1] = _temp;
                 sorted = false;
             }
         }
-        if (sorted){
+        if (sorted) {
             return;
         }
     }
@@ -19,7 +87,7 @@ function InsertSort(tab) {
     for (let i = 1; i < tab.length; i++) {
         let key = tab[i];
         let j = i - 1;
-        
+
         while (j >= 0 && tab[j] > key) {
             tab[j + 1] = tab[j];
             j = j - 1;
@@ -45,4 +113,4 @@ function SelectionSort(tab) {
     }
 }
 
-module.exports = {BubbleSort, InsertSort, SelectionSort};
+module.exports = { sum, largestElement, removeDuplicats, toUpper, meanValue, isPalindrome, fibonacci, BubbleSort, InsertSort, SelectionSort };
--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,43 @@
-const { BubbleSort, InsertSort, SelectionSort } = require('./functions.js');
+const { sum, largestElement, removeDuplicats, toUpper, meanValue, isPalindrome, fibonacci, BubbleSort, InsertSort, SelectionSort } = require('./functions.js');
+
+test("sum test", () => {
+    expect(sum(1, 5)).toBe(6);
+})
+
+test("largestElement test", () => {
+    let tab = [1, -1, 5, 15, 33, -20, 70];
+    expect(largestElement(tab)).toEqual(70);
+});
+
+test("removeDuplicats test", () => {
+    let tab = [1, 1, 1, 3];
+    expect(removeDuplicats(tab)).toEqual([1, 3]);
+});
+
+test("toUpper test", () => {
+    let text = "mini";
+    expect(toUpper(text)).toBe("MINI");
+})
+
+test("meanValue test", () => {
+    let tab = [1, 2, 3, 4, 5];
+    expect(meanValue(tab)).toBe(3);
+    expect(meanValue([])).toBe(0);
+})
+
+test("isPalindrome test", () => {
+    expect(isPalindrome("kajak")).toBe(true);
+    expect(isPalindrome("Kajak")).toBe(false);
+    expect(isPalindrome("kajaki")).toBe(false);
+})
+
+test("fibonacci test", () => {
+    expect(fibonacci(0)).toBe(1);
+    expect(fibonacci(1)).toBe(1);
+    expect(fibonacci(2)).toBe(2);
+    expect(fibonacci(3)).toBe(3);
+    expect(fibonacci(4)).toBe(5);
+})
 
 test("BubbleSort test", () => {
     let tab = [1, -1, 5, 15, 33, -20, 70];
@@ -15,6 +54,5 @@ test("InsertSort test", () => {
 test("SelectionSort test", () => {
     let tab = [1, -1, 5, 15, 33, -20, 70];
     SelectionSort(tab);
-    console.log(tab);
     expect(tab).toEqual([-20, -1, 1, 5, 15, 33, 70]);
 });
